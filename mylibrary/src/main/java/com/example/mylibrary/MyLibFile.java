8c154addbfc8759abc3505989c4a40495ddcc2ab
Deepak Sharma <Deepak.Sharma@Deepak-Sharma.wdadc.com>
2021-04-01 19:12:44 +0530

    aar second commit

--- mylibrary/src/main/java/com/example/mylibrary/MyLibFile.java
+++ mylibrary/src/main/java/com/example/mylibrary/MyLibFile.java
@@ -3,9 +3,13 @@ package com.example.mylibrary;
 import android.content.Context;
 import android.widget.Toast;
 
+import com.jio.healthdevice.JioHealthDeviceManager;
+
 public class MyLibFile {
+    public static JioHealthDeviceManager deviceManager;
 
     public static void showMsg(Context c){
-        Toast.makeText(c,"ShowMyToastMsg", Toast.LENGTH_SHORT).show();
+        deviceManager = JioHealthDeviceManager.getInstance(c);
+        Toast.makeText(c,"ShowMyToastMsg"+deviceManager.toString(), Toast.LENGTH_SHORT).show();
     }
 }
